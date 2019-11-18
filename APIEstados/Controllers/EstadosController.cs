d66a3b0315961bd3b3d0445b8bc642cd486b1e63
Renato Groffe <renato.groffe@yahoo.com.br>
2019-11-17 21:14:13 -0300

    ASP.NET Core 3.0 + Dapper + Novo Provider SQL + SQL Server + One-to-One
    ASP.NET Core 3.0 + Dapper + Novo Provider SQL + SQL Server + One-to-One

---
+++ APIEstados/Controllers/EstadosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Dapper;
+using APIEstados.Models;
+
+namespace APIEstados.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EstadosController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Estado> Get(
+            [FromServices]IConfiguration config)
+        {
+            using (SqlConnection conexao = new SqlConnection(
+                config.GetConnectionString("DadosGeograficos")))
+            {
+                return conexao.Query<Estado, Regiao, Estado>(
+                    "SELECT * " +
+                    "FROM dbo.Estados E " +
+                    "INNER JOIN dbo.Regioes R ON R.IdRegiao = E.IdRegiao " +
+                    "ORDER BY E.NomeEstado",
+                    map: (estado, regiao) =>
+                    {
+                        estado.DadosRegiao = regiao;
+                        return estado;
+                    },
+                    splitOn: "SiglaEstado,IdRegiao");
+            }
+        }
+    }
+}
